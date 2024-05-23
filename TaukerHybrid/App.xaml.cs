101b0d7472f6280b6f34af07e6a76cdc7105cd71
muhammadsaleh14 <m.salehjz@gmail.com>
2024-05-23 14:30:41 +0500

    Add project files.

---
+++ TaukerHybrid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TaukerHybrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ TaukerHybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TaukerHybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
